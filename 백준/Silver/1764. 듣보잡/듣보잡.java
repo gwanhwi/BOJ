d3ea07df5d3c8fe11495e6ad3f5f49b2107d6831
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-08-16 23:25:00 +0900

    [Silver IV] Title: 듣보잡, Time: 244 ms, Memory: 26592 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        HashMap<String,Integer> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        int count=0;
+        ArrayList<String> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            //list.add(br.readLine());
+            map.put(br.readLine(),0);
+        }
+        for (int i = 0; i < m; i++) {
+            String str = br.readLine();
+            if(map.containsKey(str)){
+                answer.add(str);
+                count++;
+            }
+        }
+        Collections.sort(answer);
+        bw.write(Integer.toString(count));
+        bw.newLine();
+        for(int i=0;i<answer.size();i++){
+            bw.write(answer.get(i));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
