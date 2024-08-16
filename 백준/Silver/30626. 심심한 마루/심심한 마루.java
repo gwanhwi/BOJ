ad0dd6f79993514b32662491597510c61fc2a770
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-08-17 02:14:29 +0900

    [Silver I] Title: 심심한 마루, Time: 1084 ms, Memory: 197784 KB -BaekjoonHub

--- 백준/Silver/30626. 심심한 마루/심심한 마루.java
+++ 백준/Silver/30626. 심심한 마루/심심한 마루.java
@@ -4,25 +4,25 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr=new int[360];
+        boolean[] arr=new boolean[360];
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             for (int j = 1; j <= b; j++) {
-                arr[a]++;
-                arr[(a + j) % 360]++;
+                arr[a]=true;
+                arr[(a + j) % 360]=true;
                 if ((a - j) < 0) {
-                    arr[360 - (j - a)]++;
+                    arr[360 - (j - a)]=true;
                 } else {
-                    arr[a - j]++;
+                    arr[a - j]=true;
                 }
             }
         }
         int count=0;
         for(int i=0;i<360;i++){
-            if(arr[i]>0)
+            if(arr[i])
                 count++;
         }
         System.out.println(count);
