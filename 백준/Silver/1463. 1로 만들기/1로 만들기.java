8f9c63ffb87ab6fa3d515fdf55f383887f577d22
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-08-22 01:40:13 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,21 @@
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr=new int[1000001];
-        arr[1]=0;
-        arr[2]=1;
-        arr[3]=1;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=4;i<=1000000;i++){
-            if(i%3==0){
-                list.add(arr[i/3]+1);
-            }
-            if(i%2==0){
-                list.add(arr[i/2]+1);
-            }
-            list.add(arr[i-1]+1);
-            arr[i]=Collections.min(list);
-            list.clear();
+        int[] arr = new int[n + 1];
+        arr[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            if (i % 6 == 0) {
+                arr[i] = Math.min(arr[i - 1], Math.min(arr[i / 2], arr[i / 3])) + 1;
+            } else if (i % 3 == 0) {
+                arr[i] = Math.min(arr[i / 3], arr[i - 1]) + 1;
+            } else if (i % 2 == 0) {
+                arr[i] = Math.min(arr[i / 2], arr[i - 1]) + 1;
+            } else arr[i] = arr[i - 1] + 1;
         }
         System.out.println(arr[n]);
     }
