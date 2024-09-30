131752c15af26f60d6dfec96c4c3fb0ea66f8968
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-10-01 02:28:54 +0900

    [level 1] Title: K번째수, Time: 0.23 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        ArrayList<Integer> arrList = new ArrayList<>();
+        for(int i=0;i<commands.length;i++){
+            for(int j=commands[i][0]-1;j<commands[i][1];j++){
+                arrList.add(array[j]);
+            }
+            Collections.sort(arrList);
+            answer[i]=arrList.get(commands[i][2]-1);
+            arrList.clear();
+        }
+        return answer;
+    }
+}
