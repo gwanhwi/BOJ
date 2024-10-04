d69146e105bebf39bf92334f3e25388217201be1
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-10-04 09:10:57 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 41.54 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,17 +3,27 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(arr[0]);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i] != arr[i-1] )
-                list.add(arr[i]);
-        }
-        int[] answer = new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            answer[i]=list.get(i);
-        }
+//         ArrayList<Integer> list = new ArrayList<>();
+//         list.add(arr[0]);
+//         for(int i=1;i<arr.length;i++){
+//             if(arr[i] != arr[i-1] )
+//                 list.add(arr[i]);
+//         }
+//         int[] answer = new int[list.size()];
+//         for(int i=0;i<list.size();i++){
+//             answer[i]=list.get(i).intValue();
+//         }
 
+//         return answer;
+        Deque<Integer> deque= new ArrayDeque<>();
+        for(int i=0;i<arr.length;i++){
+            if(deque.size()==0 || deque.peekLast()!=arr[i])
+                deque.offerLast(arr[i]);
+        }
+        int[] answer = new int[deque.size()];
+        for(int i=0;!deque.isEmpty();i++){
+            answer[i]= deque.pollFirst();
+        }
         return answer;
     }
 }
