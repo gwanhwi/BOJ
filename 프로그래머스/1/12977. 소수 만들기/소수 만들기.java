b30027d4cb9e03df8a32a270a94a7a2c21275395
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-10-05 20:39:25 +0900

    [level 1] Title: 소수 만들기, Time: 2.21 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -14,7 +14,7 @@ class Solution {
         return answer;
     }
     public boolean sosu(int num){
-        for(int i=2;i<num;i++){
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 return false;
             }
