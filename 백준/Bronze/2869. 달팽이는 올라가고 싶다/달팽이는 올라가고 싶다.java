e4c0307ae813181a05819491e6330a43ae14c5a4
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-11-07 23:23:05 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int oneDayUp = A-B;
+
+        System.out.println((int)Math.ceil((double)(V-A)/oneDayUp)+1);
+    }
+
+}
