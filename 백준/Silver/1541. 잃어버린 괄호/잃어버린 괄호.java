17bfff22392dd46001ff4e0ad5133af9fc73a1c8
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-11-14 03:49:50 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -11,37 +11,15 @@ public class Main {
         //StringTokenizer st = new StringTokenizer(br.readLine());
         //int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        List<String> operatorList = new ArrayList<>();
-        List<String> operandList = new ArrayList<>();
-        List<String> plusList = new ArrayList<>();
-        int index=0;
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='+' ||str.charAt(i)=='-'){
-                operandList.add(str.substring(index,i));
-                operatorList.add(str.substring(i,i+1));
-                index=i+1;
+        String[] minusArr = str.split("-");
+        int sum=0;
+        for(int i=0;i<minusArr.length;i++){
+            String[] plusArr = minusArr[i].split("\\+");
+            for(String plus : plusArr){
+                if(i==0) sum+= Integer.parseInt(plus);
+                else sum-=Integer.parseInt(plus);
             }
         }
-        operandList.add(str.substring(index,str.length()));
-        System.out.println(calculate(operandList,operatorList));
-    }
-    public static int calculate(List<String> operandList,List<String> operatorList){
-        List<String> list = new ArrayList<>();
-        int operandIdx=0;
-        for(int i=0;i<operatorList.size();i++){
-            if(operatorList.get(i).equals("+")){
-                int sum=Integer.parseInt(operandList.get(operandIdx))+Integer.parseInt(operandList.get(operandIdx+1));
-                operandList.set(operandIdx,Integer.toString(sum));
-                operandList.remove(operandIdx+1);
-            }
-            else if(operatorList.get(i).equals("-")){
-                operandIdx++;
-            }
-        }
-        int sum=Integer.parseInt(operandList.get(0));
-        for(int i=1;i<operandList.size();i++){
-            sum-= Integer.parseInt(operandList.get(i));
-        }
-        return sum;
+        System.out.println(sum);
     }
 }
