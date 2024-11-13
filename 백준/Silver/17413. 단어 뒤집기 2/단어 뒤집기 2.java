82f752278aefe2b3c5c70374037eccf2ba75b063
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-11-14 05:23:21 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 172 ms, Memory: 16768 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -7,35 +7,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        //StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine()," <>",true);
         boolean isTag=false;
-        String answer="";
         String tmp="";
         while(st.hasMoreTokens()){
             String str = st.nextToken();
             if(str.equals("<")){
                 isTag=true;
-                answer+=str;
+                sb.append(str);
                 continue;
             }
             if(str.equals(">")){
                 isTag=false;
-                answer+=str;
+                sb.append(str);
                 continue;
             }
             if(isTag){
-                answer+=str;
+                sb.append(str);
                 continue;
             }
             if(str.equals(" ")){
-                answer+=str;
+                sb.append(str);
                 continue;
             }
             for(int i=str.length()-1;i>=0;i--){
-                answer+=str.charAt(i);
+                sb.append(str.charAt(i));
             }
         }
-        System.out.println(answer);
+        System.out.println(sb);
     }
 }
