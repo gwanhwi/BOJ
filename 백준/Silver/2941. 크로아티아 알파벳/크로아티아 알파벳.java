4243c268b9e289ccca1b6fdf69f67109d6bb5c86
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-11-14 00:28:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -12,24 +12,10 @@ public class Main {
         //int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
         String[] croaAlpha = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-        int count=0;
-        for(int i=0;i<str.length();i++){
-            char c = str.charAt(i);
-            if(i+1<str.length() && (c=='c' || c=='d'||c=='l' || c=='n' || c=='s' || c=='z')){
-                String tmp = ""+c+str.charAt(i+1);
-                if(tmp.equals("dz") && i+2<str.length()) tmp+=str.charAt(i+2);
-                for(int j=0;j<croaAlpha.length;j++){
-                    if(tmp.equals(croaAlpha[j])){
-                        count++;
-                        i+=tmp.length()-1;
-                        break;
-                    }
-                    if(j==croaAlpha.length-1) count++;
-                }
-            }
-            else count++;
+        for(String croaWord : croaAlpha){
+            str = str.replace(croaWord,"1");
         }
-        System.out.println(count);
+        System.out.println(str.length());
 
     }
 }
