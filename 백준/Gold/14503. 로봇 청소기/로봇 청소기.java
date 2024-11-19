58c3896e33291dcfdcb65e8bc7f041fed76d2f1f
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-11-20 07:21:44 +0900

    [Gold V] Title: 로봇 청소기, Time: 108 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,97 +2,50 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 public class Main {
+    static int[][] move ={{-1,0,+1,0},{0,+1,0,-1}}; //북동남서
+    static int N;
+    static int M;
+    static int count=0;
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         //BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N=Integer.parseInt(st.nextToken());
-        int M=Integer.parseInt(st.nextToken());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         int d = Integer.parseInt(st.nextToken());//0:북, 1:동, 2:남, 3:서
-        int[][] arr = new int[N][M];
+        arr = new int[N][M];
         for(int i=0;i<N;i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<M;j++){
                 arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        int[][] move ={{1,-1,0,0},{0,0,1,-1}};
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {r,c});
-        int count=0;
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            int nowRow=now[0];
-            int nowCol=now[1];
-            if(arr[nowRow][nowCol]==0) count++;
-            arr[nowRow][nowCol]=3; //3은 청소된 방
-
-            boolean hasDirtyRoom=false;
-            int nextRow=-1;
-            int nextCol=-1;
-            for(int i=0;i<4;i++){
-                nextRow=nowRow+move[0][i];
-                nextCol=nowCol+move[1][i];
-                if(nextRow<0 || nextRow>=N || nextCol<0 || nextCol>=M || arr[nextRow][nextCol]==1)continue;
-                if(arr[nextRow][nextCol]==0){ //빈 칸이 있는 경우
-                    hasDirtyRoom=true;
-                }
-            }
-            if(hasDirtyRoom){ // 빈 칸이 있는 경우
-                do {
-                    d = (d + 3) % 4;
-                    switch (d) {
-                        case 0:
-                            nextRow = nowRow - 1;
-                            nextCol = nowCol;
-                            break;
-                        case 1:
-                            nextRow = nowRow;
-                            nextCol = nowCol + 1;
-                            break;
-                        case 2:
-                            nextRow = nowRow + 1;
-                            nextCol = nowCol;
-                            break;
-                        case 3:
-                            nextRow = nowRow;
-                            nextCol = nowCol - 1;
-                            break;
-                    }
-                } while (arr[nextRow][nextCol]!=0);
-            }
-            else {// 빈 칸이 없는 경우
-                switch (d){
-                    case 0:
-                        nextRow=nowRow+1;
-                        nextCol=nowCol;
-                        break;
-                    case 1:
-                        nextRow=nowRow;
-                        nextCol=nowCol-1;
-                        break;
-                    case 2:
-                        nextRow=nowRow-1;
-                        nextCol=nowCol;
-                        break;
-                    case 3:
-                        nextRow=nowRow;
-                        nextCol=nowCol+1;
-                        break;
-                }
-            }
-
-            if(nextRow<0 || nextRow>=N || nextCol<0 || nextCol>=M || arr[nextRow][nextCol]==1){
-                break;
-            }
-            else{
-                queue.offer(new int[] {nextRow, nextCol});
-            }
-        }
+        dfs(r,c,d);
         System.out.println(count);
+    }
+    public static void dfs(int r, int c, int d){
+        if(arr[r][c]==0) {
+            arr[r][c]=3;
+            count++;
+        }
+        for(int i=0;i<4;i++){
+            d=(d+3)%4; //반시계 90도 회전
+            int nextRow=r+move[0][d];
+            int nextCol=c+move[1][d];
+            if(nextRow<0 || nextRow>=N || nextCol<0 || nextCol>=M || arr[nextRow][nextCol]!=0) continue;
+            dfs(nextRow,nextCol,d); //빈 칸이 있는경우
+            return;
+        }
+        //빈 칸이 없는 경우
+        int backD=(d+2)%4;
+        int nextRow=r+move[0][backD];
+        int nextCol=c+move[1][backD];
+        if(nextRow<0 || nextRow>=N || nextCol<0 || nextCol>=M || arr[nextRow][nextCol]==1) return;
+        dfs(nextRow,nextCol,d);
     }
 }
