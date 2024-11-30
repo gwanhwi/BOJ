b5ac024d57a5585a139ac85b662b7c4544291e4b
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-12-01 03:06:25 +0900

    [Silver II] Title: 색종이 만들기, Time: 140 ms, Memory: 15700 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -21,44 +21,24 @@ class Main {
                 paper[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        bfs(N,0,0);
+        dividePaper(N,0,0);
         sb.append(countWhite).append("\n").append(countBlue);
         System.out.println(sb);
     }
-    static void bfs(int n, int r, int c){
-        Queue<int[]> q = new LinkedList<>();
-        visited[r][c]=true;
-        q.offer(new int[] {r,c});
-        int countOne=0;
-        int countZero=0;
-        while(!q.isEmpty()){
-            int[] now = q.poll();
-            int nowRow = now[0];
-            int nowCol = now[1];
-            if(paper[nowRow][nowCol]==0) {
-                countZero++;
-            }
-            if(paper[nowRow][nowCol]==1) {
-                countOne++;
-            }
-            for(int i=0;i<4;i++){
-                int nextRow = nowRow + move[0][i];
-                int nextCol = nowCol + move[1][i];
-                if(nextRow<r || nextRow>=n+r || nextCol<c || nextCol>=n+c || visited[nextRow][nextCol]) continue;
-                visited[nextRow][nextCol]=true;
-                q.offer(new int[] {nextRow,nextCol});
-            }
-            if(countOne*countZero!=0){
-                visited = new boolean[N][N];
-
-                bfs(n/2, r,c);
-                bfs(n/2, r,c + n/2);
-                bfs(n/2, r + n/2,c);
-                bfs(n/2, r + n/2,c + n/2);
-                return;
+    static void dividePaper(int n, int r, int c){
+        int startColor = paper[r][c];
+        for(int i=r;i<n+r;i++){
+            for(int j=c;j<n+c;j++){
+                if(paper[i][j]!=startColor){
+                    dividePaper(n/2, r,c);
+                    dividePaper(n/2, r,c + n/2);
+                    dividePaper(n/2, r + n/2,c);
+                    dividePaper(n/2, r + n/2,c + n/2);
+                    return;
+                }
             }
         }
-        if(countZero>0) countWhite++;
-        if(countOne>0) countBlue++;
+        if(startColor==0) countWhite++;
+        if(startColor==1) countBlue++;
     }
 }
