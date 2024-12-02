5a4c81b17d81f6ec764f4f72a3f5a8efb37e7844
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-12-03 03:37:04 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 112 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static List<Integer>[] graph;
+    static int[][] graph;
     static int N;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -10,57 +10,46 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        graph=new ArrayList[N+1];
+        graph=new int[N+1][N+1];
         for(int i=1;i<=N;i++){
-            graph[i]= new ArrayList<>();
+            for(int j=1;j<=N;j++){
+                graph[i][j]=Integer.MAX_VALUE;
+                if(i==j) graph[i][j]=0;
+            }
         }
         for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            graph[u].add(v);
-            graph[v].add(u);
+            graph[u][v]=1;
+            graph[v][u]=1;
         }
+        floydWarshall();
         int min=Integer.MAX_VALUE;
-        int[] kevinCount = new int[N+1];
+        int[] kevinCount=new int[N+1];
         for(int i=1;i<=N;i++){
-            kevinCount[i]=dijkstra(i);
+            for(int j=1;j<=N;j++){
+                kevinCount[i]+=graph[i][j];
+            }
             min=Math.min(min,kevinCount[i]);
         }
         for(int i=1;i<=N;i++){
-            if(kevinCount[i]==min){
+            if(kevinCount[i]==min) {
                 sb.append(i);
                 break;
             }
         }
         System.out.println(sb);
     }
-    public static int dijkstra(int start){
-        int[] dist = new int[N+1];
-        boolean[] visited = new boolean[N+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[start] =0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)-> o1[1]-o2[1]);
-        pq.offer(new int[] {start,dist[start]});
-        while(!pq.isEmpty()){
-            int[] now = pq.poll();
-            int nowUser = now[0];
-            int nowCost = now[1];
-            visited[nowUser]=true;
-            for(int v : graph[nowUser]){
-                if(visited[v]) continue;
-                if(nowCost+1<dist[v]) {
-                    dist[v] = nowCost+1;
-                    pq.offer(new int[] {v,dist[v]});
+    public static void floydWarshall(){
+        for(int k=1;k<=N;k++){
+            for(int i=1;i<=N;i++){
+                for(int j=1;j<=N;j++){
+                    if(graph[i][k] < Integer.MAX_VALUE && graph[k][j] < Integer.MAX_VALUE){
+                        graph[i][j]= Math.min(graph[i][j], graph[i][k]+graph[k][j]);
+                    }
                 }
             }
         }
-        int sum=0;
-        for(int i=1;i<=N;i++){
-            if(dist[i]<Integer.MAX_VALUE){
-                sum+=dist[i];
-            }
-        }
-        return sum;
     }
 }
