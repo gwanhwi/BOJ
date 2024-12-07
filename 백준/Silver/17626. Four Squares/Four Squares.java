6ddc4977193746588e1691d2353f8da10aaac993
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-12-07 12:33:30 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 14684 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=Math.min(dp[i],dp[i-j*j]+1); //i-j*j 의 값에 j*j 를 더하는 경우
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
