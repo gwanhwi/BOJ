b4b1dfe3e90c3105bae2e90fc4bcbd0b178a0004
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-12-26 22:15:22 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -8,18 +8,18 @@ class Main {
         int N = Integer.parseInt(br.readLine());
         int[] T = new int[N+1];
         int[] P = new int[N+1];
-        int[] dp = new int[N+2];
+        int[] dp = new int[N+1];
         for(int i=1;i<=N;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             T[i] =Integer.parseInt(st.nextToken());
             P[i] =Integer.parseInt(st.nextToken());
         }
-        for(int i=N;i>0;i--){
-            dp[i]=dp[i+1];
-            if(i+T[i]<N+2){
-                dp[i]=Math.max(P[i]+dp[i+T[i]],dp[i]);
+        for(int i=1;i<=N;i++){
+            dp[i]=Math.max(dp[i],dp[i-1]);
+            if(i+T[i]-1<N+1){
+                dp[i+T[i]-1] = Math.max(dp[i+T[i]-1],dp[i-1]+P[i]);
             }
         }
-        System.out.println(dp[1]);
+        System.out.println(dp[N]);
     }
 }
