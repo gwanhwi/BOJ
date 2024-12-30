7a1af43c71291f5ce49859ba1cc7febf01c35968
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2024-12-30 23:10:19 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 16788 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -2,33 +2,33 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int N;
-    static int M;
-    static int count=0;
-    static int[] arr;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[N];
+        int[] arr = new int[N];
         for(int i=0;i<N;i++){
             arr[i]=Integer.parseInt(st.nextToken());
         }
-        combination(0,0,0);
-        System.out.println(count);
-    }
-    public static void combination(int start,int depth,int sum){
-        if(depth==2){
-            if(sum==M){
+        Arrays.sort(arr);
+        int count=0;
+        int left=0;
+        int right=N-1;
+        while(left<right){
+            if(arr[left]+arr[right]==M) {
                 count++;
+                left++;
+                right--;
             }
-            return;
-
+            else if(arr[left]+arr[right]>M){
+                right--;
+            }
+            else{
+                left++;
+            }
         }
-        for(int i=start;i<N;i++){
-            combination(i+1,depth+1,sum+arr[i]);
-        }
+        System.out.println(count);
     }
 }
