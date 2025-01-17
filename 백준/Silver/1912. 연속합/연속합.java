410fa97ead352413f2eaf2238c3f5de3caf7320a
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2025-01-17 20:09:42 +0900

    [Silver II] Title: 연속합, Time: 232 ms, Memory: 22544 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -5,23 +5,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=n;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        int[] dp = new int[n+1];
 
-        for(int i=1;i<=n;i++){
-            if(dp[i-1]<0){
-                dp[i]=arr[i];
-                continue;
-            }
-            dp[i]=dp[i-1]+arr[i];
-        }
+        int[] dp = new int[n+1];
         int max=Integer.MIN_VALUE;
         for(int i=1;i<=n;i++){
+            int num = Integer.parseInt(st.nextToken());
+            dp[i]=Math.max(num,dp[i-1]+num);
             max=Math.max(max,dp[i]);
+
         }
         System.out.println(max);
     }
