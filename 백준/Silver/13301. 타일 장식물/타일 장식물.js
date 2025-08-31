eeb0c78d7e254d3b87ab53a500062e88f11cefda
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2025-08-31 23:18:25 +0900

    [Silver V] Title: 타일 장식물, Time: 100 ms, Memory: 9336 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.js
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const input = Number(fs.readFileSync('/dev/stdin').toString().trim());
 
 const N = input;
-const F = Array(N + 2).fill(0);
+const F = Array(N + 2).fill(0n);
 
-F[1] = 1;
-F[2] = 1;
+F[1] = 1n;
+F[2] = 1n;
 
 for (let i = 3; i <= N + 1; i++) {
   F[i] = F[i - 1] + F[i - 2];
 }
 
-const result = 2 * (F[N] + F[N + 1]);
-console.log(result);
+const result = 2n * (F[N] + F[N + 1]);
+console.log(result.toString());
