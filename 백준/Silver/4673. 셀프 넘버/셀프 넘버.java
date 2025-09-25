24c8512f96696b6d425712716340a47aa4b9e226
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2025-09-25 23:09:10 +0900

    [Silver V] Title: 셀프 넘버, Time: 108 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean[] isGenerated = new boolean[10001];
+
+        for (int i = 1; i <= 10000; i++) {
+            int n = d(i);
+            if (n <= 10000) {
+                isGenerated[n] = true;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10000; i++) {
+            if (!isGenerated[i]) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+
+    public static int d(int n) {
+        int sum = n;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
