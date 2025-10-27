8ad5d9c020677894436930f0c160ebd5ce4f48d1
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2025-10-27 22:13:09 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1344 ms, Memory: 110016 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+        for (int i = N - 1; i >= 0; i--) sb.append(arr[i]).append('\n');
+        System.out.print(sb);
+    }
+}
